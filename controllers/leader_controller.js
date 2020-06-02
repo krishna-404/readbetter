ab7ff5f01fe41cbb615acf0becfd35edf4dd5769
Krishna Agarwal <agarwalbalkrishnaindia@gmail.com>
2020-06-02 10:57:51 +0530

    height of image container

--- controllers/leader_controller.js
+++ controllers/leader_controller.js
@@ -1,6 +1,6 @@
 const leader = require('../models/leader_model');
 
-Function LeaderModel(){
+function LeaderModel(){
 
     this.getLeader = function (req,res){
         leader_name = req.params.leader;
